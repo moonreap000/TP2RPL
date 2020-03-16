4aacad7c37eeef852ad95c47a12f6d5db6566d05
moonreap000 <moonreap000@gmail.com>
2020-03-16 20:40:07 +0700

    TP2

--- app.js
+++ app.js
@@ -1,60 +1,80 @@
-//Problem: Hints are shown even when form is valid
-//Solution: Hide and show them at appropriate times
-var $password = $("#password");
-var $confirmPassword = $("#confirm_password");
+var DB_USER = [	
+	{
+		username: "",
+		password: ""
+	}
+]
 
-//Hide hints
-$("form span").hide();
+var inputUsername = document.querySelector("#username");
+var inputPassword = document.querySelector("#password");
+var inputKonfirmasi_password = document.querySelector("#konfirmasi_password");
+var buttonSubmit = document.querySelector("#submitForm");
+/*
+console.log(inputUsername);
+console.log(inputPassword);
+console.log(buttonSubmit);*/
+var warningmsg = document.getElementById("toggle_p");
+warningmsg.style.display = "none";
 
-function isPasswordValid() {
-  return $password.val().length > 8;
-}
+var warningmsg_username = document.getElementById("toggle_username");
+warningmsg_username.style.display = "none";
 
-function arePasswordsMatching() {
-  return $password.val() === $confirmPassword.val();
-}
+var warningmsg_password = document.getElementById("toggle_password");
+warningmsg_password.style.display = "none";
 
-function canSubmit() {
-  return isPasswordValid() && arePasswordsMatching();
-}
+buttonSubmit.addEventListener('click', (e)=>{
+	e.preventDefault();
+	var valueInputPassword = inputPassword.value;
+	var valueInputUsername = inputUsername.value;
+	var valueInputKonfirmasi_password = inputKonfirmasi_password.value;
+		
+	let flag=0;
+	if (valueInputPassword === valueInputKonfirmasi_password) {
+		flag = 1;
+	}
+	else{
+		flag = 0;
+	}
 
-function passwordEvent() {
-  //Find out if password is valid
-  if (isPasswordValid()) {
-    //Hide hint if valid
-    $password.next().hide();
-  } else {
-    //else show hint
-    $password.next().show();
-  }
-}
+	let x ={}
 
-function confirmPasswordEvent() {
-  //Find out if password and confirmation match
-  if (arePasswordsMatching()) {
-    //Hide hint if match
-    $confirmPassword.next().hide();
-  } else {
-    //else show hint
-    $confirmPassword.next().show();
-  }
-}
+	/*push*/
+	if (valueInputUsername==="") {
+		warningmsg_username.style.display = "block";
+		if(valueInputPassword===""){
+			warningmsg_password.style.display = "block";
+		}
+		else{
+			warningmsg_password.style.display = "none";
+		}
+		console.log("Tidak masuk ke array")
+	}
 
-function enableSubmitEvent() {
-  $("#submit").prop("disabled", !canSubmit());
-}
+	else if(valueInputPassword==="" && valueInputUsername!=""){
+		warningmsg_password.style.display = "block";
+		warningmsg_username.style.display = "none";
+	}
+	else{
+		warningmsg_username.style.display = "none";
+		warningmsg_password.style.display = "none";
+		if (flag === 0) {
+			warningmsg.style.display = "block";
+			console.log("Tidak masuk ke array")
+		}
+		else{
+			warningmsg.style.display = "none";
+			alert("Selamat anda masuk ke home");
+			console.log("Masuk ke array")
 
-//When event happens on password input
-$password
-  .focus(passwordEvent)
-  .keyup(passwordEvent)
-  .keyup(confirmPasswordEvent)
-  .keyup(enableSubmitEvent);
-
-//When event happens on confirmation input
-$confirmPassword
-  .focus(confirmPasswordEvent)
-  .keyup(confirmPasswordEvent)
-  .keyup(enableSubmitEvent);
-
-enableSubmitEvent();
+			x ={
+				username : valueInputUsername,
+				password : valueInputPassword
+				}
+			
+			DB_USER.push(x);
+			
+			console.log(DB_USER);
+		}
+	}
+	
+})
